6655a7a7893c111f6e50cfa0efb089d4bd409fff
Abdunabi <abdunabiabduvahobov86@gmail.com>
2023-05-05 21:49:07 +0500

    feat:side-menu

--- src/components/layout/navbar/sidebar/index.jsx
+++ src/components/layout/navbar/sidebar/index.jsx
@@ -22,14 +22,14 @@ const index = ({setSideBar}) => {
             <X className="fill-black" />
           </div>
         </div>
-        <div className="flex flex-col space-y-1 justify-center group cursor-pointer">
+        <div className="flex flex-col space-y-1 justify-center group [&:hover>.sub-menu]:flex cursor-pointer">
           <div className="flex items-center justify-between w-full">
             <p className="group-hover:text-primary duration-300">Categories</p>
             <div className="p-3 bg-gray rounded-[12px]">
               <DropdownIcon className="fill-black/50 group-hover:fill-primary duration-300 group-hover:rotate-0 -rotate-90" />
             </div>
           </div>
-          <div className="hidden group-hover:flex flex-col space-y-1 text-lg w-[100%] z-10 bg-white rounded-md shadow-md p-2">
+          <div className="sub-menu hidden flex-col space-y-1 text-lg w-[100%] z-10 bg-white rounded-md shadow-md p-2">
             <p className="hover:text-primary">Sub menu one</p>
             <p className="hover:text-primary">Sub menu two</p>
             <p className="hover:text-primary">Sub menu three</p>
